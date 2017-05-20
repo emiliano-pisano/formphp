6c8a57d3ee327fa3b45e36a7bfa9274f71170478
emiliano-pisano <pisanoemiliano@gmail.com>
2017-05-20 19:37:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+  <div class="jumbotron">
+        
+    <!-- navbar -->
+    <?php include("fragments/nav.html"); ?>
+
+    <!-- welcome -->
+    <?php include("fragments/welcome.html"); ?>
+
+  </div>
+
+   <!-- form -->
+   <?php include("fragments/form.html"); ?>
+
+          </body>
+        <script type="text/javascript" src="vendors/js/jquery.min.js"></script>
+        <script type="text/javascript" src="vendors/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/js/bootstrapValidator.min.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        </html>
